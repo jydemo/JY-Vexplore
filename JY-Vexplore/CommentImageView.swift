189f5e258b16a02461db5c42bf74148522d441cc
jydemo <jydemo@qq.com>
2017-09-04 19:29:14 +0800

    CommentImageView处理评论中的头像。

---
+++ JY-Vexplore/CommentImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CommentImageView.swift
+//  JY-Vexplore
+//
+//  Created by JYKit on 2017/9/3.
+//  Copyright © 2017年 atom. All rights reserved.
+//
+
+import UIKit
+
+class CommentImageView: UIImageView {
+    var imageURL: String?
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
