457310ef7b0e46cb0614afd39246e78361d239e4
jydemo <jydemo@qq.com>
2017-09-04 19:23:04 +0800

    在protocols文件新增SwipCellDelegate、NotificationCellDelegate和CommentCellDelegate协议

--- JY-Vexplore/Protocols.swift
+++ JY-Vexplore/Protocols.swift
@@ -20,3 +20,29 @@ protocol TopicCellDelegate: AvatarTappedDelegate{
 protocol TopicDetailDelegate:  TopicCellDelegate {
     func favorieBtnTapped()
 }
+
+protocol SwipCellDelegate: class {
+    func cellWillBeginSwipe(at indexPath: IndexPath)
+    func cellShouldBeginSwipe() -> Bool
+}
+
+extension SwipCellDelegate {
+    func cellShouldBeginSwipe() -> Bool{
+        return true
+    }
+}
+
+protocol NotificationCellDelegate: AvatarTappedDelegate,SwipCellDelegate {
+    func deleteNotification(withID notificationID: String)
+}
+
+protocol CommentCellDelegate: AvatarTappedDelegate, SwipCellDelegate {
+    func thankBtnTapped(withReplyID replyID: String, indexPath: IndexPath)
+    func igonreBtnTapped(withReplyID replyID: String)
+    func replyBtnTapped(withUsername username: String)
+    func longPress(at  indexPath: IndexPath)
+}
+
+
+
+
